f4ef7269fd6d798203263cd3ce0288c9c38b6b28
Vitamin-JS <Vpopchenkov@gmail.com>
2020-08-06 00:34:25 +0300

    Add решение задачи 2.5

--- js/homework02.js
+++ js/homework02.js
@@ -127,3 +127,35 @@
 
 // =================  ЗАДАЧА 2-5 =================================================
 // ===============================================================================
+
+// Функция предикат
+// Функция предикат возвращает true или false
+
+// Напиши функцию checkForSpam(message), принимающую 1 параметр message - строку.Функция проверяет ее на
+// содержание слов spam и sale.Если нашли запрещенное слово то функция возвращает true, если
+// запрещенных слов нет функция возвращает false.Слова в строке могут быть в произвольном регистре.
+
+// -----------------    Решение   --------------------------------------------------
+
+// function checkForSpam(str) {
+//   "use strict";
+//   // Write code under this line
+//   let checkString = str.toLowerCase();
+//   console.log(checkString);
+//   let resultCheck = false;
+
+//   if (checkString.includes("spam") || checkString.includes("sale")) {
+//     console.log("Advertise");
+//     resultCheck = true;
+//   }
+
+//   return resultCheck;
+// }
+
+// console.log(checkForSpam("Latest technology news")); // false
+
+// console.log(checkForSpam("JavaScript weekly newsletter")); // false
+
+//console.log(checkForSpam("Get best sale offers now!")); // true
+
+// console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
